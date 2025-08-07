a06cb94f6ccf21aee3e119fd29b4f48b4b4f107e
vibesengineering <205912982+vibesengineering@users.noreply.github.com>
2025-08-07 09:48:53 +0000

    Code generation: 4f0b5279

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,5 @@
 export const PROJECT_TITLE = "fartcaster";
-export const PROJECT_DESCRIPTION = "by @alexpaden";
+export const PROJECT_DESCRIPTION = "Fart on your Farcaster friends with custom profile picture bubbles";
 export const PROJECT_CREATOR = "alexpaden";
 export const PROJECT_AVATAR_URL =
   "https://wrpcd.net/cdn-cgi/imagedelivery/BXluQx4ige9GuW0Ia56BHw/37460dae-996e-481a-9c2c-0374da719400/anim=false,fit=contain,f=auto,w=576";
